b27083753bc3d492b3d9263e94472586ca99eee8
Alvarg27 <alvargbesne@gmail.com>
2022-04-29 00:15:25 -0500

    update

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,11 @@
 import React from "react";
+import { CgMenuGridO } from "react-icons/cg";
 
 export default function Header() {
   return (
     <div className="bg-white h-20 flex justify-between px-4 drop-shadow-md">
       <p className="text-blue-700 text-4xl font-bold mx-0 my-auto">tectify</p>
-      <ul className="flex mx-0 my-auto">
-        <li className="mx-3 hover:text-blue-700 transition duration-300 cursor-pointer">
-          Servicios
-        </li>
-        <li className="mx-3 hover:text-blue-700 transition duration-300 cursor-pointer">
-          Precios
-        </li>
-        <li className="mx-3 hover:text-blue-700 transition duration-300 cursor-pointer">
-          Contacto
-        </li>
-      </ul>
+      <CgMenuGridO className="my-auto mx-0 text-5xl" />
     </div>
   );
 }
