463b049849e628ecf82bf9560a7e0844e114378f
Alvarg27 <alvargbesne@gmail.com>
2022-04-29 16:27:35 -0500

    Update Security.js

--- components/Security.js
+++ components/Security.js
@@ -6,23 +6,24 @@ export default function Security() {
     <div className="px-4 py-12 flex flex-col">
       <FaLock className="text-blue-700 m-auto text-5xl font-bold" />
       <h2 className="text-5xl font-bold mb-6 text-center ">
-        Protegemos tu información y la de tus usuarios
+        Más seguridad que nunca
       </h2>
       <h2 className="text-3xl mb-6 text-center text-gray-500">
-        Diseñamos protocolos de seguridad robustos para proteger un informacion
-        y la de tus usuarios antes de que sea demasiado tarde
+        Diseñamos protocolos de seguridad robustos para{" "}
+        <b>proteger tu información</b> y la de tus usuarios,{" "}
+        <b>antes de que sea demasiado tarde.</b>
       </h2>
 
       <h2 className=" mb-6 text-center text-gray-500">
-        Cada día son hackeados al rededor de 30,000 sitios web.
+        <b>Cada día son hackeados</b> al rededor de <b>30,000 sitios</b> web.
       </h2>
       <h2 className=" mb-6 text-center text-gray-500">
-        Los costos de robo de datos globales fueron de USD 4,24 millones de
-        dolares en 2021
+        <b>Los costos de robo de datos</b> globales fueron de{" "}
+        <b>4,24 millones de dolares</b> en 2021.
       </h2>
       <h2 className=" mb-6 text-center text-gray-500">
-        WordPress representó alrededor del 90% de los sitios web hackeados en
-        2018.
+        <b>WordPress representó</b> alrededor del{" "}
+        <b>90% de los sitios web hackeados</b> en 2018.
       </h2>
     </div>
   );
