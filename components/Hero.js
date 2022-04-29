37a5a939c9359929fc2a60e69d1fb3cf09e5b9ec
Alvarg27 <alvargbesne@gmail.com>
2022-04-29 13:59:25 -0500

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div className="w-full h-screen flex flex-col">
-      <div className="m-auto flex flex-col  px-10">
+    <div className="w-full h-screen flex flex-col overflow-hidden">
+      <div className="m-auto flex flex-col">
         <h2 className="bg-clip-text text-transparent text-6xl font-bold m-auto text-center bg-gradient-to-tl from-[#a6c0fe] to-[#f68084]">
           Soluciones tecnológicas para el futuro
         </h2>
-        <p className="text-center mt-10 text-gray-500">
+        <p className="text-center mt-10 text-gray-500 px-8">
           <b>tectify</b> busca impulsar la transición tecnológica de las
           empresas e instituciones con el objetivo de de optimizar sus procesos
           administrativos, logísticos y operativos. Logrando así mantenerlas
