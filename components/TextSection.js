f3776f8ee2efe35ae0ee508ccd32a0ddbc1d9ef4
Alvarg27 <alvargbesne@gmail.com>
2022-04-29 00:41:59 -0500

    Update TextSection.js

--- components/TextSection.js
+++ components/TextSection.js
@@ -4,10 +4,10 @@ export default function TextSection() {
   return (
     <div className="w-full h-[600px] m bg-blue-700 rounded-t-3xl flex px-4">
       <div className="m-auto  text-center">
-        <h2 className="text-white text-6xl font-bold mb-6">
+        <h2 className="text-white text-4xl font-bold mb-6">
           ¡Haciendo posible lo imposible!
         </h2>
-        <h3 className="text-white text-2xl">
+        <h3 className="text-white text-xl">
           Tú decides cómo hacer crecer tu empresa, <b>tectify</b> lo vuelve
           realidad
         </h3>
