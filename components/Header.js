c3f1db71afa3024eb6d896e8d5e8d5dde837dd5b
Alvarg27 <alvargbesne@gmail.com>
2022-04-29 02:03:50 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { CgMenuGridO } from "react-icons/cg";
 
 export default function Header() {
   return (
     <div className="bg-white h-[90px] flex justify-between px-4 drop-shadow-md">
       <p className="text-blue-700 text-4xl font-bold mx-0 my-auto">tectify</p>
+      <CgMenuGridO className="my-auto mx-0 text-5xl text-gray-600" />
     </div>
   );
 }
