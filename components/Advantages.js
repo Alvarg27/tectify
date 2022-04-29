b6d2e9a8db43e8247e9cb3c5c8eec12e45ad83f3
Alvarg27 <alvargbesne@gmail.com>
2022-04-29 13:55:29 -0500

    update

--- components/Advantages.js
+++ components/Advantages.js
@@ -7,25 +7,50 @@ export default function Advantages() {
       id: 1,
       title: "React",
       image: "/react-logo.webp",
-      description: "Reinventa la experiencia de usuario",
+      description: "Reinventa la experiencia de usuario.",
+      website: "https://reactjs.org/",
     },
     {
       id: 2,
       title: "Next",
       image: "/next-logo.webp",
-      description: "Acelera la carga de tu página web",
+      description: "Acelera la carga de tu página web.",
+      website: "https://nextjs.org/",
     },
     {
       id: 3,
       title: "Tailwind",
       image: "/tailwind-logo.webp",
-      description: "Diseño de paginas web inovador",
+      description: "Diseño de paginas web inovador.",
+      website: "https://tailwindcss.com/",
+    },
+    {
+      id: 5,
+      title: "Sanity",
+      image: "/sanity-logo.webp",
+      description: "Panel de administrador fácil de editar.",
+      website: "https://www.sanity.io/",
     },
     {
       id: 4,
       title: "Node",
       image: "/node-logo.webp",
-      description: "Contola la lógica de negocios",
+      description: "Contola la lógica de negocios.",
+      website: "https://nodejs.dev/",
+    },
+    {
+      id: 5,
+      title: "MongoDB",
+      image: "/mongo-logo.webp",
+      description: "Almacena tus bases de datos.",
+      website: "https://www.mongodb.com/",
+    },
+    {
+      id: 6,
+      title: "Digital Ocan",
+      image: "/digital-ocean-logo.webp",
+      description: "Despliega tus aplicaciones de forma eficaz.",
+      website: "https://www.digitalocean.com/",
     },
   ];
   return (
@@ -34,11 +59,11 @@ export default function Advantages() {
         Somos los mejores en lo que hacemos.
       </h2>
 
-      <h2 className="text-3xl font-bold mb-6 text-center">
+      <h2 className="text-3xl font-bold mb-6 text-center text-gray-500">
         Dile adios a wordpress y reinventa tu empresa con las tecnologías más
         modernas desarrolladas a tu medida.
       </h2>
-      <div className="flex m-auto overflow-x-scroll">
+      <div className="flex overflow-x-auto my-auto">
         {technologies.map((technology) => (
           <TechnologyCard key={technology.id} technology={technology} />
         ))}
--- components/TechnologyCard.js
+++ components/TechnologyCard.js
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function TechnologyCard({ technology }) {
   return (
-    <div>
-      <div className="rounded-lg shadow-lg mx-1 my-4 bg-white p-4 h-[100px] w-[100px] flex flex-col">
-        <img src={technology.image} className="w-[60px] m-auto" />
-      </div>
-    </div>
+    <Link href={technology.website}>
+      <a target="_blank">
+        <div>
+          <div className="rounded-lg shadow-lg mx-1 my-4 bg-white p-4 h-[100px] w-[100px] flex flex-col">
+            <img src={technology.image} className="w-[60px] m-auto" />
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 }
