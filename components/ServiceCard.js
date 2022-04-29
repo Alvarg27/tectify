769f9224057c2cb7aed993040fc7371e49c8273e
Alvarg27 <alvargbesne@gmail.com>
2022-04-29 02:03:07 -0500

    update

--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -1,11 +1,49 @@
 import React from "react";
+import {
+  FaUtensils,
+  FaShoppingCart,
+  FaCalendarAlt,
+  FaDatabase,
+  FaServer,
+  FaLayerGroup,
+  FaMobile,
+  FaColumns,
+  FaStore,
+} from "react-icons/fa";
 
 export default function ServiceCard({ service }) {
+  const renderIcon = () => {
+    if (service.icon === "FaShoppingCart") {
+      return <FaShoppingCart className="my-auto xm-0 text-red-400 text-2xl" />;
+    } else if (service.icon === "FaUtensils") {
+      return <FaUtensils className="my-auto xm-0 text-cyan-900 text-2xl" />;
+    } else if (service.icon === "FaCalendarAlt") {
+      return <FaCalendarAlt className="my-auto xm-0 text-green-400 text-2xl" />;
+    } else if (service.icon === "FaDatabase") {
+      return <FaDatabase className="my-auto xm-0 text-yellow-400 text-2xl" />;
+    } else if (service.icon === "FaServer") {
+      return <FaServer className="my-auto xm-0 text-purple-900 text-2xl" />;
+    } else if (service.icon === "FaLayerGroup") {
+      return <FaLayerGroup className="my-auto xm-0 text-purple-400 text-2xl" />;
+    } else if (service.icon === "FaMobileAlt") {
+      return <FaMobile className="my-auto xm-0 text-indigo-400 text-2xl" />;
+    } else if (service.icon === "FaColumns") {
+      return <FaColumns className="my-auto xm-0 text-blue-700 text-2xl" />;
+    }
+  };
   return (
     <div>
-      <div className="rounded-lg shadow-lg mx-6 my-4 bg-white p-3 h-[120px]">
-        <h3 className="font-bold">{service.title}</h3>
-        <p>{service.description}</p>
+      <div className="rounded-lg shadow-lg mx-1 my-4 bg-white p-4 h-[140px] flex flex-col">
+        <div className="my-auto">
+          <div className="flex justify-between my-auto">
+            <h3 className="font-bold">{service.title}</h3>
+            {renderIcon()}
+          </div>
+          <div className="my-auto">
+            <p className="mr-8">{service.description}</p>
+            <p className="text-blue-700 mt-1">Ver más</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Services.js
+++ components/Services.js
@@ -7,50 +7,58 @@ export default function Services() {
       id: 1,
       title: "Landing page",
       description: "Página estática informativa, de alta velocidad.",
+      icon: "FaColumns",
     },
     {
       id: 2,
       title: "Pagina Web",
       description: "Sitio web profesional para todo tipo de industrias.",
+      icon: "FaLayerGroup",
     },
     {
       id: 3,
       title: "E-commerce",
       description: "Soluciones de comercio electrónico integrales.",
+      icon: "FaShoppingCart",
     },
     {
       id: 4,
       title: "Menú de restaurante",
       description: "Menú interactivo, rápido y facil de editar.",
+      icon: "FaUtensils",
     },
     {
       id: 5,
       title: "Administrador de reservaciones",
       description:
         "Gestiona y organiza tus reservaciones de manera automatizada.",
+      icon: "FaCalendarAlt",
     },
     {
       id: 6,
       title: "Aplicaciones mobiles",
       description: "Aplicaciones mobiles especializadas a tus necesidades.",
+      icon: "FaMobileAlt",
     },
     {
       id: 7,
       title: "Desarrolo de backend",
       description:
         "Backend y servidores avanzados para resolver todo tipo de necesidades.",
+      icon: "FaServer",
     },
     {
       id: 8,
       title: "Data science",
       description:
         "Diseño de estructuras de datos y análisis estadistico de información.",
+      icon: "FaDatabase",
     },
   ];
   return (
-    <div className="white">
-      <div className="mt-20 px-4">
-        <h2 className="text-6xl font-bold mb-6 text-center">
+    <div className="bg-blue-700">
+      <div className="bg-white px-4 py-20 rounded-t-3xl ">
+        <h2 className="text-5xl font-bold mb-6 text-center">
           Conoce nuestros servicios...
         </h2>
         <p className="text-gray-500 text-center">
