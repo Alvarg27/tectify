c567ed676ef59789e2e550b9e6c11458ca9d7020
Alvarg27 <alvargbesne@gmail.com>
2022-12-17 22:41:56 -0600

    Update TCtTSHWeHC.js

--- pages/TCtTSHWeHC.js
+++ pages/TCtTSHWeHC.js
@@ -19,27 +19,35 @@ const ChristmasCard = () => {
       {destinatario && (
         <div className="min-h-screen bg-blue-500 flex items-center justify-center">
           <div className="px-10 flex flex-col justify-center items-center">
-            <p className="text-gray-200 font-medium text-[100px] mb-12 rotate-90 ">{`*<]:{)`}</p>
+            <p className="text-gray-200 font-medium text-[80px] mb-12 rotate-90 ">{`*<]:{)`}</p>
             <p className=" text-center text-gray-200 font-medium text-xl">
               ¡Feliz navidad y año nuevo!
             </p>
-            <div className="text-gray-500 my-4 font-medium bg-white p-6 rounded-xl shadow-lg">
+            <div className="text-gray-500 my-4 bg-white p-6 rounded-xl shadow-lg">
               <p>{`{`}</p>
-              <div className="ml-4">
-                <p>{`"fecha": "${moment().format("DD/MM/YY")}"`}</p>
-                <p>{`"destinatario": "${destinatario}"`}</p>
+              <div className="ml-4 text-sm">
+                <div className="flex">
+                  <p>{`"destinatario":`}</p>
+                  <p>{`"${destinatario}"`}</p>
+                </div>
                 <div className="flex">
                   <p>{`"mensaje":`}</p>
                   <p>{` "Mis mejores deseos para estas fiestas"`}</p>
                 </div>
-
-                <p>{`"remitente": "Alvar García B."`}</p>
+                <div className="flex">
+                  <p>{`"remitente":`}</p>
+                  <p>{` "Alvar García Besné"`}</p>
+                </div>
+                <div className="flex">
+                  <p>{`"fecha":`}</p>
+                  <p>{`"${moment().format("DD/MM/YY")}""`}</p>
+                </div>
               </div>
               <p>{`}`}</p>
             </div>
             <p
               onClick={() => router.push("/")}
-              className="text-gray-200 mt-10 font-bold cursor-pointer mx-auto"
+              className="text-gray-200 mt-4 font-bold cursor-pointer mx-auto"
             >
               tectify
             </p>
